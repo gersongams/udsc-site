c08f39e09344bd37da1381339968c7258ced9627
Gerson Garrido <ggarridom@uni.pe>
2018-05-28 11:45:07 -0500

    favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,56 @@
 module.exports = {
-  siteMetadata: {
-    title: "UDSC websilte"
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+    siteMetadata: {
+        title: "UDSC websilte"
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
-}
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: "pages"
+            }
+        },
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                path: `${__dirname}/src/img`,
+                name: "images"
+            }
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-transformer-remark",
+            options: {
+                plugins: []
+            }
+        },
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/img/favicon/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false
+                }
+            }
+        },
+        {
+            resolve: "gatsby-plugin-netlify-cms",
+            options: {
+                modulePath: `${__dirname}/src/cms/cms.js`
+            }
+        },
+        "gatsby-plugin-netlify" // make sure to keep it last in the array
+    ]
+};
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,16 +7,37 @@ import "./all.sass";
 import Footer from "../components/Footer";
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="UDSC"/>
-    <Navbar/>
-    <div>{children()}</div>
-    <Footer/>
-  </div>
+    <div>
+        <Helmet>
+            <html lang="es"/>
+            <meta charSet="utf-8"/>
+            <meta http-equiv="x-ua-compatible"
+                  content="ie=edge"/>
+            <meta name="viewport"
+                  content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <title>UDSC</title>
+
+            <link rel="canonical"
+                  href="http://mysite.com/example"/>
+            <meta property="og:title"
+                  content="UDSC - UNI Data Science Community"/>
+            <meta name="description"
+                  content="Comunidad de Data Science de la UNI"/>
+            <meta property="og:type"
+                  content="article"/>
+            <meta name="keywords"
+                  content="UNI, Data Science, Machine Learning, Vision Computacional, Big Data, Ciencia de Datos, NLP, Hackathon, Datathon, "/>
+
+
+        </Helmet>
+        <Navbar/>
+        <div>{children()}</div>
+        <Footer/>
+    </div>
 );
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func
+    children: PropTypes.func
 };
 
 export default TemplateWrapper;
